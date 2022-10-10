f10b05c91ab9c577803b55476c0346a6590abb3b
lawrence <lawrence@LAPTOP-DRN90NTL>
2022-10-10 23:50:22 +0800

    Add project files.

---
+++ FacialRecognition/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace FacialRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(40, 42);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(349, 336);
+            this.pic.TabIndex = 0;
+            this.pic.TabStop = false;
+            this.pic.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pic2
+            // 
+            this.pic2.Location = new System.Drawing.Point(458, 42);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(349, 336);
+            this.pic2.TabIndex = 2;
+            this.pic2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 399);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 69);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "DETECT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(289, 399);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(205, 69);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "CAPTURE";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(536, 399);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(205, 69);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "SAVE";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 588);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.pic);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
